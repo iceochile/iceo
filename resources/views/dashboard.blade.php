90d95173457111c8820882bb59ff9ff87b390d5e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-06 01:33:36 -0400

    update home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -89,17 +89,17 @@
                   </span>
                 </div>
                 <div class="mt-6">
-                  <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Better understand your customers</h2>
+                  <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Pagina Web / Carrito de Compras</h2>
                   <p class="mt-4 text-lg text-gray-500">Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.</p>
                   <div class="mt-6">
-                    <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"> Get started </a>
+                    <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"> Contratar</a>
                   </div>
                 </div>
               </div>
             </div>
             <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-              <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-                <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg" alt="Customer profile user interface">
+              <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-4/6 h">
+                <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="https://okhosting.com/resources/uploads/2014/08/tipos-de-paginas-web1.jpg" alt="Customer profile user interface">
               </div>
             </div>
           </div>
