ceaa69ccab0fe5ef6f094b0f0024c3491bbea731
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-06 00:51:32 -0400

    update logo 2

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,3 +1,3 @@
 <div class="flex">
-    <img class=" h-10 w-14 object-cover" src="https://icones.pro/wp-content/uploads/2021/04/icone-internet-noire.png" alt=""><h1 class="text-center font-bold text-3xl items-center">I-CEO</h1>
+    <img class=" h-10 w-14 object-cover mr-1" src="https://icones.pro/wp-content/uploads/2021/04/icone-internet-noire.png" alt=""><h1 class="text-center font-bold text-3xl items-center">I-CEO</h1>
 </div>
