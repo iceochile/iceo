dac6c24904627ed1d3ebd9cb1335f061ec21bcf3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-06 00:15:31 -0400

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <div class="max-w-7xl mx-auto relative bg-white overflow-hidden mt-16">
+    <div class="max-w-7xl mx-auto relative bg-white overflow-hidden mt-8">
         <div class="max-w-7xl mx-auto">
           <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
