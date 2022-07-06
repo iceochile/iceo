db3c2319d731817ef866fbcafa395d289cd1c240
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-05 20:42:59 -0400

    Revert "traducir login"
    This reverts commit fb3a67860ff2736a2403aaf0451ccd7c6afa0d87.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,10 +27,10 @@
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Ingresar</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
                 </div>
