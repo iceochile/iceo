7d00cc59c9b24b70c36dccc6981dc193fdb6a680
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-05 22:03:48 -0400

    logo update

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,6 +1,6 @@
 <a href="/">
     <figure>
-        <img class=" h-14 w-30 my-4 object-contain" src="{{asset('home/logo.png')}}" alt="">
+        <img class=" h-14 w-24 my-4 object-contain" src="{{asset('home/logo.png')}}" alt="">
     </figure>
       
 </a>
