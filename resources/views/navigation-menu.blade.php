d2b376b2eea745e801fdc254cb6b00cc1052c0a7
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-06 03:37:17 -0400

    update

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('¿Como funciona?') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Productos') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -155,6 +158,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('¿Como Funciona?') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                {{ __('Productos') }}
             </x-jet-responsive-nav-link>
         </div>
 
