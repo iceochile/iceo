3ccbefec1f0fd5d8d3c34b62f94a303d36ede073
iceochile <iceochile@gmail.com>
2022-07-06 01:44:23 -0400

    up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
               <div class="sm:text-center lg:text-left pt-0 md:pt-10">
                 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                   <span class="block xl:inline">Project Management</span>
-                  <span class="block text-indigo-600 xl:inline">Administra Tu empresa de Forma Online</span>
+                  <span class="block text-indigo-600 xl:inline">Controla Tu empresa de Forma Online</span>
                 </h1>
                 <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Creamos la intranet que tu empresa necesita, ya sea una simple pagina web o carrito de compras, hasta soluciones a medida que se ajusten a las necesidades de su modelo de negocio.</p>
                 <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
