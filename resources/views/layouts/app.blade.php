2356029f13086534b866a48447da5e869be47622
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-08 20:30:35 -0400

    ipdate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="shortcut icon" href="{{asset('home/icono.ico')}}">
         <!-- Styles -->
         @livewireStyles
 
