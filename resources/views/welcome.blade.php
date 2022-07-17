a02c774f78974331bc9b3e762642411093320e0f
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-17 18:45:42 -0400

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                     <h3 class="text-gray-900 text-sm font-medium truncate">RidersChilenos</h3>
                     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">Activo</span>
                     </div>
-                    <p class="mt-1 text-gray-500 text-sm truncate">$403.260</p>
+                    <p class="mt-1 text-gray-500 text-sm truncate">$1.403.260</p>
                 </div>
                 <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
                 </div>
@@ -59,7 +59,7 @@
                     <h3 class="text-gray-900 text-sm font-medium truncate">PetFansChile</h3>
                     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">Activo</span>
                     </div>
-                    <p class="mt-1 text-gray-500 text-sm truncate">1.230.000</p>
+                    <p class="mt-1 text-gray-500 text-sm truncate">$1.230.000</p>
                 </div>
                 <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
                 </div>
