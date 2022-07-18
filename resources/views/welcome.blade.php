ac68a2716b31ba083d4bc4b0b93d3dc4fadd7c6c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-17 23:13:34 -0400

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,7 +109,7 @@
                     <div class="-ml-px w-0 flex-1 flex mt-10">
                     <a href="tel:+1-202-555-0170" class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
                         <!-- Heroicon name: solid/phone -->
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                         </svg>
                         <span class="ml-3">Agregar mi negocio</span>
                     </a>
