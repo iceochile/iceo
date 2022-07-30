546bea318af3cac7535f556df1d72b66da39da31
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-30 19:45:27 -0400

    update test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 <div class="w-full flex items-center justify-between p-6 space-x-6">
                 <div class="flex-1 truncate">
                     <div class="flex items-center space-x-3">
-                    <h3 class="text-gray-900 text-sm font-medium truncate">RidersChilenos</h3>
+                    <h3 class="text-gray-900 text-sm font-medium truncate">RidersChileno</h3>
                     <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">Activo</span>
                     </div>
                     <p class="mt-1 text-gray-500 text-sm truncate">$1.403.260</p>
